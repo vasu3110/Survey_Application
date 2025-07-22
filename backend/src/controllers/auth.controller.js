ec5940fd3ad69300475c38dd8cf8b14214cab94f
Vasu Sharma <vasu22300@gmail.com>
2025-07-22 23:55:23 +0530

    Add SameSite attribute to cookie options in loginUser

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -89,7 +89,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "None",
     };
 
     return res
